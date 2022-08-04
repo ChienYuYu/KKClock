2567f6e54af7e6a153c7f07b56dfdeb29b6e2d9c
陳建佑 <97875966+ChienYuYu@users.noreply.github.com>
2022-08-04 15:03:37 +0800

    0804

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/kkClock/dist/'
+  // publicPath: '/kkClock/dist/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/kkclock/'
+    : '/',
 });
