a91c0b16ef0f2d067b653373cf855ed8853eae6f
陳建佑 <jianyou224@gmail.com>
2022-10-10 14:03:04 +0800

    優化

--- src/components/CarouselView.vue
+++ src/components/CarouselView.vue
@@ -1,55 +1,32 @@
 <template>
   <div class="bg-white pb-5 pt-5">
-    <div ref="carousel" id="carouselExampleCaptions" class="carousel slide carousel-fade container"
+    <div ref="carousel" id="carouselExampleCaptions"
+    class="carousel index-carousel slide carousel-fade container"
       data-bs-ride="carousel">
       <div class="carousel-inner rounded-3">
+        <div class="carousel-caption">
+          <h5 class="mb-0 slogan rounded-3">
+            kkClock<br />鐘的專家 最專業時鐘專賣<br />
+          </h5>
+          <button class="btn go-buy">
+            &ensp;
+            <i class="bi bi-cart me-1" />
+            <span>來去逛逛</span>
+            <i class="bi bi-caret-right" />
+            <i class="bi bi-caret-right" />
+          </button>
+        </div>
         <router-link to="product_list/全部商品" class="carousel-item active">
           <img src="../assets/img/banner04.jpg"
-          class="banner d-block w-100" alt="banner1" height="400" />
-          <div class="carousel-caption">
-            <h5 class="mb-0 slogan rounded-3">
-              kkClock<br />鐘的專家 最專業時鐘專賣<br />
-            </h5>
-            <button class="btn go-buy">
-              &ensp;
-              <i class="bi bi-cart me-1" />
-              <span>來去逛逛</span>
-              <i class="bi bi-caret-right" />
-              <i class="bi bi-caret-right" />
-            </button>
-          </div>
+          class="banner d-block w-100" alt="banner1" height="500" />
         </router-link>
         <router-link to="product_list/全部商品" class="carousel-item">
           <img src="../assets/img/banner07.jpg"
-          class="banner d-block w-100" alt="banner2" height="400" />
-          <div class="carousel-caption">
-            <h5 class="mb-0 slogan rounded-3">
-              kkClock<br />鐘的專家 最專業時鐘專賣<br />
-            </h5>
-            <button class="btn go-buy">
-              &ensp;
-              <i class="bi bi-cart me-1" />
-              <span>來去逛逛</span>
-              <i class="bi bi-caret-right" />
-              <i class="bi bi-caret-right" />
-            </button>
-          </div>
+          class="banner d-block w-100" alt="banner2" height="500" />
         </router-link>
         <router-link to="product_list/全部商品" class="carousel-item">
           <img src="../assets/img/banner08.jpg"
-          class="banner d-block w-100" alt="banner3" height="400" />
-          <div class="carousel-caption">
-            <h5 class="mb-0 slogan rounded-3">
-              kkClock<br />鐘的專家 最專業時鐘專賣<br />
-            </h5>
-            <button class="btn go-buy">
-              &ensp;
-              <i class="bi bi-cart me-1" />
-              <span>來去逛逛</span>
-              <i class="bi bi-caret-right" />
-              <i class="bi bi-caret-right" />
-            </button>
-          </div>
+          class="banner d-block w-100" alt="banner3" height="500" />
         </router-link>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
@@ -78,7 +55,7 @@ export default {
   methods: {},
   mounted() {
     this.carousel = new Carousel(this.$refs.carousel, {
-      interval: 3500,
+      interval: 1500,
     });
   },
 };
@@ -89,7 +66,13 @@ export default {
   object-fit: cover;
 }
 
+.index-carousel {
+  position: relative;
+}
+
 .carousel-caption {
+  position: absolute;
+  z-index: 99;
   right: 0;
   left: 0;
   bottom: 0;
